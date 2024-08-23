b1900e0008041a9e98528e03eb99e2639034464c
Arpan Dey <midecl.arpan@gmail.com>
2024-08-23 16:51:43 +0530

    Fraction Addition and Subtraction

---
+++ 23-08-24.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string fractionAddition(string expression) {
+        int num = 0;
+        int denom = 1;
+
+        int i = 0;
+        while (i < expression.size()) {
+            int currNum = 0;
+            int currDenom = 0;
+
+            bool isNegative = false;
+
+            if (expression[i] == '-' || expression[i] == '+') {
+                if (expression[i] == '-') {
+                    isNegative = true;
+                }
+                i++;
+            }
+
+            while (isdigit(expression[i])) {
+                int val = expression[i] - '0';
+                currNum = currNum * 10 + val;
+                i++;
+            }
+
+            if (isNegative) currNum *= -1;
+
+            i++;
+
+            while (i < expression.size() && isdigit(expression[i])) {
+                int val = expression[i] - '0';
+                currDenom = currDenom * 10 + val;
+                i++;
+            }
+
+            num = num * currDenom + currNum * denom;
+            denom = denom * currDenom;
+        }
+
+        int gcd = abs(FindGCD(num, denom));
+
+        num /= gcd;
+        denom /= gcd;
+
+        return to_string(num) + "/" + to_string(denom);
+    }
+
+private:
+    int FindGCD(int a, int b) {
+        if (a == 0) return b;
+        return FindGCD(b % a, a);
+    }
+};
