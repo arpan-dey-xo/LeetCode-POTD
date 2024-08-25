1193ac234bece375c4f9f1c0c5f06326c43034a4
Arpan Dey <midecl.arpan@gmail.com>
2024-08-25 21:00:25 +0530

    Binary Tree Postorder Traversal

---
+++ 25-08-24.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void postorderTraversalHelper(TreeNode* currentNode, vector<int>& result) {
+        if (!currentNode) {
+            return;
+        }
+        postorderTraversalHelper(currentNode->left, result);
+        postorderTraversalHelper(currentNode->right, result);
+        result.push_back(currentNode->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        postorderTraversalHelper(root, result);
+        return result;
+    }
+};
