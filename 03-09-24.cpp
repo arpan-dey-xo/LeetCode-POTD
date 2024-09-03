95fb153af1f4ab6ed26e44348986e444efc46d3e
Arpan Dey <midecl.arpan@gmail.com>
2024-09-03 21:39:05 +0530

    Sum of Digits of String After Convert

---
+++ 03-09-24.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string numericString = "";
+        for (char ch : s) {
+            numericString += to_string(ch - 'a' + 1);
+        }
+
+        while (k--) {
+            int digitSum = 0;
+            for (char digit : numericString) {
+                digitSum += digit - '0';
+            }
+            numericString = to_string(digitSum);
+        }
+
+        return stoi(numericString);
+    }
+};
