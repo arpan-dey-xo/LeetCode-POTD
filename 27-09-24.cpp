1a335fd71adefd7af3a553d981bbdfc8ac73c4f3
Arpan Dey <midecl.arpan@gmail.com>
2024-09-27 20:02:12 +0530

    My Calendar II

---
+++ 27-09-24.cpp
@@ -0,0 +1,33 @@
+class MyCalendarTwo {
+public:
+    map<int, int> bookingCount;
+    int maxOverlappedBooking;
+
+    MyCalendarTwo() { maxOverlappedBooking = 2; }
+
+    bool book(int start, int end) {
+        bookingCount[start]++;
+        bookingCount[end]--;
+
+        int overlappedBooking = 0;
+        for (pair<int, int> bookings : bookingCount) {
+            overlappedBooking += bookings.second;
+
+            if (overlappedBooking > maxOverlappedBooking) {
+                bookingCount[start]--;
+                bookingCount[end]++;
+
+                if (bookingCount[start] == 0) {
+                    bookingCount.erase(start);
+                }
+                if (bookingCount[end] == 0) {
+                    bookingCount.erase(end);
+                }
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
