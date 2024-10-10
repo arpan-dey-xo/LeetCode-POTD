3d7464d79d130b9987850bd61f83127490564e7e
Arpan Dey <midecl.arpan@gmail.com>
2024-10-10 19:47:09 +0530

    Maximum Width Ramp

---
+++ 10-10-24.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> rightMax(n);
+
+        rightMax[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = max(rightMax[i + 1], nums[i]);
+        }
+
+        int left = 0, right = 0;
+        int maxWidth = 0;
+
+        while (right < n) {
+            while (left < right && nums[left] > rightMax[right]) {
+                left++;
+            }
+            maxWidth = max(maxWidth, right - left);
+            right++;
+        }
+
+        return maxWidth;
+    }
+};
