46ab1d334390453fbb0dbc7706547c44321cbfaf
Arpan Dey <midecl.arpan@gmail.com>
2024-12-06 21:55:55 +0530

    Maximum Number of Integers to Chose From a Range

---
+++ 06-12-24.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        sort(banned.begin(), banned.end());
+
+        int bannedIdx = 0, count = 0;
+        for (int num = 1; num <= n && maxSum >= 0; num++) {
+            if (bannedIdx < banned.size() && banned[bannedIdx] == num) {
+                while (bannedIdx < banned.size() && banned[bannedIdx] == num) {
+                    bannedIdx++;
+                }
+            }
+            else {
+                maxSum -= num;
+                if (maxSum >= 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
