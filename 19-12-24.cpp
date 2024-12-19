d710657e93865c6d3b26be71e6b2376625644f87
Arpan Dey <midecl.arpan@gmail.com>
2024-12-19 21:43:47 +0530

    Max Chunks To Make Sorted

---
+++ 19-12-24.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        int chunks = 0, prefixSum = 0, sortedPrefixSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefixSum += arr[i];
+            sortedPrefixSum += i;
+            if (prefixSum == sortedPrefixSum) {
+                chunks++;
+            }
+        }
+        return chunks;
+    }
+};
