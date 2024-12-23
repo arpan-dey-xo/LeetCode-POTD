96ecabefa41f66f269a5babe0a79e7bf0f1a6a87
Arpan Dey <midecl.arpan@gmail.com>
2024-12-23 19:52:43 +0530

    Minimum Number of Operations to Sort a Binary Tree by Level

---
+++ 23-12-24.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    const int SHIFT = 20;
+    const int MASK = 0xFFFFF;
+
+public:
+    int minimumOperations(TreeNode* root) {
+        queue<TreeNode*> queue;
+        queue.push(root);
+        int swaps = 0;
+
+        while (!queue.empty()) {
+            int levelSize = queue.size();
+            vector<long long> nodes(levelSize);
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* node = queue.front();
+                queue.pop();
+                nodes[i] = (static_cast<long long>(node->val) << SHIFT) + i;
+
+                if (node->left != nullptr) queue.push(node->left);
+                if (node->right != nullptr) queue.push(node->right);
+            }
+
+            sort(nodes.begin(), nodes.end());
+
+            for (int i = 0; i < levelSize; i++) {
+                int origPos = static_cast<int>(nodes[i] & MASK);
+                if (origPos != i) {
+                    swap(nodes[i], nodes[origPos]);
+                    swaps++;
+                    i--;
+                }
+            }
+        }
+        return swaps;
+    }
+};
