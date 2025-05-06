130154f67100e932bae520a98b12d3626ef2da11
Arpan Dey <midecl.arpan@gmail.com>
2025-05-06 23:19:45 +0530

    Build Array from Permutation

---
+++ 06-05-25.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        vector<int> buildArray(vector<int>& nums) {
+            int n = nums.size();
+            for (int i = 0; i < n; ++i) {
+                nums[i] += 1000 * (nums[nums[i]] % 1000);
+            }
+            for (int i = 0; i < n; ++i) {
+                nums[i] /= 1000;
+            }
+            return nums;
+        }
+    };
